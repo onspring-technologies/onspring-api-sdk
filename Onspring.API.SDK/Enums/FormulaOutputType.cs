a9a60c083ecdf92ccfae8566830758918db132b8
StevanFreeborn <sfreeborn7@gmail.com>
2022-12-10 22:46:53 -0600

    fix: Changed named value Number in FormulaOutputType enum to Numeric which is the actual value that the Onspring API will return for formula fields with their output type configured as numeric. NOTE: All tests now passing again.

--- Onspring.API.SDK/Enums/FormulaOutputType.cs
+++ Onspring.API.SDK/Enums/FormulaOutputType.cs
@@ -11,7 +11,7 @@ namespace Onspring.API.SDK.Enums
     public enum FormulaOutputType
     {
         Text = 0,
-        Number = 1,
+        Numeric = 1,
         DateAndTime = 2,
         List = 3,
     }
