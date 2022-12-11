e2ca7a1eb80db9e5d4ddc8ea67d2c5b2bd01e5d9
StevanFreeborn <sfreeborn7@gmail.com>
2022-12-10 22:31:51 -0600

    fix: Modifed the FieldsController.cs for the test server to not return a field test data based on concrete classes and enums from the sdk itself. Instead the fields returned are generic objects that represent a field data set you'd get back from the api. This way I can add additional fields to the dataset that demonstrate the FormulaOutputType enum contains incorrect values for date and number.

--- Onspring.API.SDK.Tests/TestServer/Controllers/FieldsController.cs
+++ Onspring.API.SDK.Tests/TestServer/Controllers/FieldsController.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace Onspring.API.SDK.Tests.TestServer.Controllers
 {
-  [ApiController, Route("[controller]")]
+    [ApiController, Route("[controller]")]
     [ExcludeFromCodeCoverage]
     [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Just a test controller. Not an API that gets shipped.")]
     [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Just a test controller. Not an API that gets shipped.")]
