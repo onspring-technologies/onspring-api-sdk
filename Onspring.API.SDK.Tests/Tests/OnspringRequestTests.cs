3bbd1d90b5737809232b0cddb531451ffdcb6d50
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-14 11:13:47 -0500

    chore: stub out tests for OnspringRequest model

---
+++ Onspring.API.SDK.Tests/Tests/OnspringRequestTests.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Onspring.API.SDK.Tests.Tests
+{
+  [TestClass, ExcludeFromCodeCoverage]
+  public class OnspringRequestTests
+  {
+  }
+}
