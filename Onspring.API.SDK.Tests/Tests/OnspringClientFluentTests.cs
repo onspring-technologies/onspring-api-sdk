1b6959acafb735e6909d04e5233d45c8d22a3efb
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-14 11:12:19 -0500

    test: add test to verify create request method returns an OnspringRequest

---
+++ Onspring.API.SDK.Tests/Tests/OnspringClientFluentTests.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Onspring.API.SDK.Models;
+
+namespace Onspring.API.SDK.Tests.Tests
+{
+  [TestClass, ExcludeFromCodeCoverage]
+  public class OnspringClientFluentTests
+  {
+    [TestMethod]
+    public void CreateRequest_WhenCalled_ItShouldReturnAnInstanceOfAnOnspringRequest()
+    {
+      var client = new OnspringClient("https://api.onspring.com", "apiKey");
+      var request = client.CreateRequest();
+      Assert.IsInstanceOfType(request, typeof(OnspringRequest));
+    }
+  }
+}
