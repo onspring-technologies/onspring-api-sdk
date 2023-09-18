00f324d6038af3972949b7ba9417f394b3a59c3f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-17 21:35:53 -0500

    refactor: rename format property

--- Onspring.API.SDK/Models/Fluent/GetRecordByIdRequestBuilderOptions.cs
+++ Onspring.API.SDK/Models/Fluent/GetRecordByIdRequestBuilderOptions.cs
@@ -7,6 +7,6 @@ namespace Onspring.API.SDK.Models.Fluent
     public class GetRecordByIdRequestBuilderOptions
     {
         public IEnumerable<int> FieldIds { get; set; } = Enumerable.Empty<int>();
-        public DataFormat DataFormat { get; set; } = DataFormat.Raw;
+        public DataFormat Format { get; set; } = DataFormat.Raw;
     }
 }
