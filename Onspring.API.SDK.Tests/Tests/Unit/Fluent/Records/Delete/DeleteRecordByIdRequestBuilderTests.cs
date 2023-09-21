13f9136c4c9cdd2b255f74ef89b5d55e45c751f5
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-20 20:54:30 -0500

    tests: add tests for delete records builders

--- Onspring.API.SDK.Tests/Tests/Unit/Fluent/Records/Delete/DeleteRecordByIdRequestBuilderTests.cs
+++ Onspring.API.SDK.Tests/Tests/Unit/Fluent/Records/Delete/DeleteRecordByIdRequestBuilderTests.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using Onspring.API.SDK.Models;
 using Onspring.API.SDK.Models.Fluent;
 
 namespace Onspring.API.SDK.Tests.Tests.Unit.Fluent
@@ -18,6 +20,31 @@ namespace Onspring.API.SDK.Tests.Tests.Unit.Fluent
             _builder = new DeleteRecordByIdRequestBuilder(_client, 1, 1);
         }
 
-        // TODO: Write tests
+        [TestMethod]
+        public void Constructor_WhenCalled_ItShouldReturnAnInstanceWithPropertiesSet()
+        {
+            var appId = 1;
+            var recordId = 1;
+            var builder = new DeleteRecordByIdRequestBuilder(_client, appId, recordId);
+
+            Assert.IsInstanceOfType<DeleteRecordByIdRequestBuilder>(builder);
+            Assert.AreEqual(appId, builder.AppId);
+            Assert.AreEqual(recordId, builder.RecordId);
+        }
+
+        [TestMethod]
+        public async Task SendAsync_WhenCalled_ItShouldReturnAnApiResponse()
+        {
+            var apiResponse = new ApiResponse();
+
+            _client
+                .DeleteRecordAsync(Arg.Any<int>(), Arg.Any<int>())
+                .Returns(apiResponse);
+
+            var result = await _builder.SendAsync();
+
+            Assert.AreEqual(apiResponse, result);
+        }
+
     }
 }
--- Onspring.API.SDK.Tests/Tests/Unit/Fluent/Records/Delete/DeleteRecordsByIdsRequestBuilderTests.cs
+++ Onspring.API.SDK.Tests/Tests/Unit/Fluent/Records/Delete/DeleteRecordsByIdsRequestBuilderTests.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using Onspring.API.SDK.Models;
 using Onspring.API.SDK.Models.Fluent;
 
 namespace Onspring.API.SDK.Tests.Tests.Unit.Fluent
@@ -18,6 +20,30 @@ namespace Onspring.API.SDK.Tests.Tests.Unit.Fluent
             _builder = new DeleteRecordsByIdsRequestBuilder(_client, 1, new[] { 1, 2, 3 });
         }
 
-        // TODO: Write tests
+        [TestMethod]
+        public void Constructor_WhenCalled_ItShouldReturnAnInstanceWithPropertiesSet()
+        {
+            var appId = 1;
+            var recordIds = new[] { 1, 2, 3 };
+            var builder = new DeleteRecordsByIdsRequestBuilder(_client, appId, recordIds);
+
+            Assert.IsInstanceOfType<DeleteRecordsByIdsRequestBuilder>(builder);
+            Assert.AreEqual(appId, builder.AppId);
+            Assert.AreEqual(recordIds, builder.RecordIds);
+        }
+
+        [TestMethod]
+        public async Task SendAsync_WhenCalled_ItShouldReturnAnApiResponse()
+        {
+            var apiResponse = new ApiResponse();
+
+            _client
+                .DeleteRecordsAsync(Arg.Any<DeleteRecordsRequest>())
+                .Returns(apiResponse);
+
+            var result = await _builder.SendAsync();
+
+            Assert.AreEqual(apiResponse, result);
+        }
     }
 }
