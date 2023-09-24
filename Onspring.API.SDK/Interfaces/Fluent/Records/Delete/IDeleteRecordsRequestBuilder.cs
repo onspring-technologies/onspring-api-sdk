b8eba1d456716c36dbff9aea90578e671ba486ff
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:10:04 -0500

    docs: add comments to SaveRecordRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Records/Delete/IDeleteRecordsRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Records/Delete/IDeleteRecordsRequestBuilder.cs
@@ -9,7 +9,7 @@ namespace Onspring.API.SDK.Interfaces.Fluent
         /// Specifies the app from which to delete records.
         /// </summary>
         /// <param name="appId">The ID of the app from which to delete records.</param>
-        /// <returns>A <see cref="IDeleteRecordsByAppRequestBuilder"/>  for further configuration of the request.</returns>
+        /// <returns>A <see cref="IDeleteRecordsByAppRequestBuilder"/> for further configuration of the request.</returns>
         IDeleteRecordsByAppRequestBuilder FromApp(int appId);
     }
 }
--- Onspring.API.SDK/Interfaces/Fluent/Records/Save/ISaveRecordInAppRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Records/Save/ISaveRecordInAppRequestBuilder.cs
@@ -1,8 +1,20 @@
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to save a record in an app.
+    /// </summary>
     public interface ISaveRecordInAppRequestBuilder
     {
+        /// <summary>
+        /// Gets the ID of the app in which to save the record.
+        /// </summary>
         int AppId { get; }
+
+        /// <summary>
+        /// Specifies the record to save. If the record ID is null, a new record will be created.
+        /// </summary>
+        /// <param name="recordId">The ID of the record to save.</param>
+        /// <returns>A <see cref="ISaveRecordByIdRequestBuilder"/> for further configuration of the request.</returns>
         ISaveRecordByIdRequestBuilder WithId(int? recordId);
     }
 }
