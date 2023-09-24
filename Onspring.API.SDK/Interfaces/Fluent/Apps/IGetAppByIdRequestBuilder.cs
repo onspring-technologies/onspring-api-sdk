f2f9fa714ef756e416c76d02c3a419cea0d28647
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:57:49 -0500

    docs: add comments for get apps request builders

--- Onspring.API.SDK/Interfaces/Fluent/Apps/IGetAppByIdRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Apps/IGetAppByIdRequestBuilder.cs
@@ -3,9 +3,20 @@ using Onspring.API.SDK.Models;
 
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to retrieve an app by ID.
+    /// </summary>
     public interface IGetAppByIdRequestBuilder
     {
+        /// <summary>
+        /// Gets the ID of the app to retrieve.
+        /// </summary>
         int AppId { get; }
+
+        /// <summary>
+        /// Asynchronously sends the request to retrieve the app.
+        /// </summary>
+        /// <returns>An awaitable task that returns an <see cref="ApiResponse{App}"/> when complete.</returns>
         Task<ApiResponse<App>> SendAsync();
     }
 }
--- Onspring.API.SDK/Models/Fluent/Apps/GetAppByIdRequestBuilder.cs
+++ Onspring.API.SDK/Models/Fluent/Apps/GetAppByIdRequestBuilder.cs
@@ -3,11 +3,21 @@ using Onspring.API.SDK.Interfaces.Fluent;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to retrieve an app by ID.
+    /// </summary>
+    /// <inheritdoc/>
     public class GetAppByIdRequestBuilder : IGetAppByIdRequestBuilder
     {
         private readonly IOnspringClient _client;
+
         public int AppId { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAppByIdRequestBuilder"/> class.
+        /// </summary>
+        /// <param name="client">The client used to send the request.</param>
+        /// <param name="appId">The ID of the app to retrieve.</param>
         internal GetAppByIdRequestBuilder(IOnspringClient client, int appId)
         {
             _client = client;
