e41dc5497f26b9b4ef64e9a1c269835dfc98c5d3
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:45:50 -0500

    docs: add comments to DeleteRecordRequestBuilder

--- Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsRequestBuilder.cs
+++ Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsRequestBuilder.cs
@@ -2,10 +2,18 @@ using Onspring.API.SDK.Interfaces.Fluent;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete records.
+    /// </summary>
+    /// <inheritdoc/>
     public class DeleteRecordsRequestBuilder : IDeleteRecordsRequestBuilder
     {
         private readonly IOnspringClient _client;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="DeleteRecordsRequestBuilder"/> class.
+        /// </summary>
+        /// <param name="client">The <see cref="IOnspringClient"/> used to send the request.</param>
         internal DeleteRecordsRequestBuilder(IOnspringClient client)
         {
             _client = client;
