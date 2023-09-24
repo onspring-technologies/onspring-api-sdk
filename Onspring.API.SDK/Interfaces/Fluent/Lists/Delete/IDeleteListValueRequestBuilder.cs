2e2579aa0e283e5327f45699a87a9136674df72f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:45:04 -0500

    docs: add comments to DeleteListValueRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Lists/Delete/IDeleteListValueRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Lists/Delete/IDeleteListValueRequestBuilder.cs
@@ -1,7 +1,15 @@
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete a list value.
+    /// </summary>
     public interface IDeleteListValueRequestBuilder
     {
+        /// <summary>
+        /// Specifies the ID of the list where the list value to delete is located.
+        /// </summary>
+        /// <param name="listId">The ID of the list where the list value to delete is located.</param>
+        /// <returns>A <see cref="IDeleteListValueInListRequestBuilder"/> for further configuration of the request.</returns>
         IDeleteListValueInListRequestBuilder InList(int listId);
     }
 }
--- Onspring.API.SDK/Interfaces/Fluent/Lists/Delete/IDeleteListValueWithIdRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Lists/Delete/IDeleteListValueWithIdRequestBuilder.cs
@@ -4,10 +4,25 @@ using Onspring.API.SDK.Models;
 
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete a list value in a list.
+    /// </summary>
     public interface IDeleteListValueWithIdRequestBuilder
     {
+        /// <summary>
+        /// Gets the ID of the list to delete the value in.
+        /// </summary>
         int ListId { get; }
+
+        /// <summary>
+        /// Gets the ID of the list value to delete.
+        /// </summary>
         Guid Id { get; }
+
+        /// <summary>
+        /// Asynchronously sends the request to delete the list value.
+        /// </summary>
+        /// <returns>An awaitable task that returns an <see cref="ApiResponse"/> when complete.</returns>
         Task<ApiResponse> SendAsync();
     }
 }
