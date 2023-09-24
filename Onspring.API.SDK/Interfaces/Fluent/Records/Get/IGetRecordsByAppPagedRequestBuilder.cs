dbb9dc5873adcb1ef3ddb347617a5f72736a1f75
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:36:58 -0500

    docs: add comments to GetRecordsByAppPagedRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Records/Get/IGetRecordsByAppPagedRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Records/Get/IGetRecordsByAppPagedRequestBuilder.cs
@@ -7,17 +7,68 @@ using Onspring.API.SDK.Models.Fluent;
 
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to get records by an app.
+    /// </summary>
     public interface IGetRecordsByAppPagedRequestBuilder
     {
+        /// <summary>
+        /// Gets the app from which to retrieve records.
+        /// </summary>
         int AppId { get; }
+
+        /// <summary>
+        /// Gets the page number to retrieve.
+        /// </summary>
         int PageNumber { get; }
+
+        /// <summary>
+        /// Gets the IDs of the fields to retrieve.
+        /// </summary>
         IEnumerable<int> FieldIds { get; }
+
+        /// <summary>
+        /// Gets the data format for the response.
+        /// </summary>
         DataFormat Format { get; }
+
+        /// <summary>
+        /// Gets the page size.
+        /// </summary>
         int PageSize { get; }
+
+        /// <summary>
+        /// Sends the request asynchronously.
+        /// </summary>
+        /// <returns>An awaitable task that returns an API response <see cref="ApiResponse{GetPagedRecordsResponse}"/> when complete.</returns>
         Task<ApiResponse<GetPagedRecordsResponse>> SendAsync();
+
+        /// <summary>
+        /// Sends the request asynchronously with the specified options.
+        /// </summary>
+        /// <param name="options">An action that constructs the options to use when sending the request.</param>
+        /// <returns>An awaitable task that returns an API response <see cref="ApiResponse{GetPagedRecordsResponse}"/> when complete.</returns>
         Task<ApiResponse<GetPagedRecordsResponse>> SendAsync(Action<GetRecordsByAppPagedRequestBuilderOptions> options);
+
+        /// <summary>
+        /// Specifies the page size to retrieve.
+        /// </summary>
+        /// <param name="pageSize">The size of page to retrieve</param>
+        /// <returns>A builder for further configuration of the request.</returns>
         IGetRecordsByAppPagedRequestBuilder WithPageSize(int pageSize);
+
+        /// <summary>
+        /// Specifies the IDs of the fields to retrieve.
+        /// </summary>
+        /// <param name="fieldIds">The IDs of the fields to retrieve.</param>
+        /// <returns>A builder for further configuration of the request.</returns>
         IGetRecordsByAppPagedRequestBuilder WithFieldIds(IEnumerable<int> fieldIds);
+
+        /// <summary>
+        /// Specifies the data format for the response.
+        /// </summary>
+        /// <param name="dataFormat">The data format for the response.</param>
+        /// <returns>A builder for further configuration of the request.</returns>
         IGetRecordsByAppPagedRequestBuilder WithFormat(DataFormat dataFormat);
     }
 }
