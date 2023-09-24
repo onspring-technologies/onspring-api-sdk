39b1dd1ad81f0354ed87b6ce56e45144dec1cee7
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:05:11 -0500

    docs: Add comments to QueryRecordsByAppPagedRequestBuilderOptions

--- Onspring.API.SDK/Models/Fluent/Records/Get/QueryRecordsByAppPagedRequestBuilderOptions.cs
+++ Onspring.API.SDK/Models/Fluent/Records/Get/QueryRecordsByAppPagedRequestBuilderOptions.cs
@@ -4,11 +4,29 @@ using Onspring.API.SDK.Enums;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents options for building paged requests to query records by application.
+    /// </summary>
     public class QueryRecordsByAppPagedRequestBuilderOptions
     {
+        /// <summary>
+        /// Gets or sets the page number for the paged request. Default is 1.
+        /// </summary>
         public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Gets or sets the page size for the paged request. Default is 50.
+        /// </summary>
         public int PageSize { get; set; } = 50;
+
+        /// <summary>
+        /// Gets or sets the field IDs to include in the query. Default is an empty collection.
+        /// </summary>
         public IEnumerable<int> FieldIds { get; set; } = Enumerable.Empty<int>();
+
+        /// <summary>
+        /// Gets or sets the data format for the query results. Default is <see cref="DataFormat.Raw"/> .
+        /// </summary>
         public DataFormat Format { get; set; } = DataFormat.Raw;
     }
 }
