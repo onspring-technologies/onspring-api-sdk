cea8428d3408c88f597de9346dc51585278d773b
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 10:39:54 -0500

    docs: add comments to Filter class

--- Onspring.API.SDK/Models/Filter.cs
+++ Onspring.API.SDK/Models/Filter.cs
@@ -3,13 +3,26 @@ using Onspring.API.SDK.Enums;
 
 namespace Onspring.API.SDK.Models
 {
+    /// <summary>
+    /// Represents a filter used for querying data.
+    /// </summary>
     public class Filter
     {
+        /// <summary>
+        /// Gets or sets the identifier of the field to filter on.
+        /// </summary>
         public int FieldId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filter operator to apply to the field.
+        /// </summary>
         public FilterOperator Operator { get; set; }
 
         private object filterValue;
 
+        /// <summary>
+        /// Gets or sets the value to filter against.
+        /// </summary>
         public object Value
         {
             get { return filterValue; }
@@ -24,8 +37,17 @@ namespace Onspring.API.SDK.Models
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Filter"/> class.
+        /// </summary>
         internal Filter() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Filter"/> class with specified values.
+        /// </summary>
+        /// <param name="fieldId">The identifier of the field to filter on.</param>
+        /// <param name="filterOperator">The filter operator to apply to the field.</param>
+        /// <param name="value">The value to filter against.</param>
         public Filter(int fieldId, FilterOperator filterOperator, object value)
         {
             FieldId = fieldId;
@@ -33,6 +55,10 @@ namespace Onspring.API.SDK.Models
             Value = value;
         }
 
+        /// <summary>
+        /// Returns a string representation of the filter.
+        /// </summary>
+        /// <returns>A string representing the filter.</returns>
         public override string ToString()
         {
             if (Value == null)
