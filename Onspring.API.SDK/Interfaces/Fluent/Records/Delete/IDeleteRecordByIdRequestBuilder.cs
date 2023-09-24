9df3e88b3c8300c6e1e9387949623f4209254b31
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:53:55 -0500

    docs: add comments to DeleteRecordByIdRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Records/Delete/IDeleteRecordByIdRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Records/Delete/IDeleteRecordByIdRequestBuilder.cs
@@ -3,10 +3,25 @@ using Onspring.API.SDK.Models;
 
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete a record from an app by ID.
+    /// </summary>
     public interface IDeleteRecordByIdRequestBuilder
     {
+        /// <summary>
+        /// Gets the ID of the app from which to delete the record.
+        /// </summary>
         int AppId { get; }
+
+        /// <summary>
+        /// Gets the ID of the record to delete.
+        /// </summary>
         int RecordId { get; }
+
+        /// <summary>
+        /// Asynchronously sends the request to delete the record.
+        /// </summary>
+        /// <returns>An awaitable task that returns an <see cref="ApiResponse"/> when complete.</returns>
         Task<ApiResponse> SendAsync();
     }
 }
