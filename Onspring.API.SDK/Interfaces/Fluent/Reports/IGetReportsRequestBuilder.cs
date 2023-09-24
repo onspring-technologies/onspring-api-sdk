dcadafd11ce314d1c5192728f8490595996db942
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:20:31 -0500

    docs: add comments to GetReportsRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Reports/IGetReportsRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Reports/IGetReportsRequestBuilder.cs
@@ -1,7 +1,15 @@
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to get reports.
+    /// </summary>
     public interface IGetReportsRequestBuilder
     {
+        /// <summary>
+        /// Specifies the ID of the app from which to get reports.
+        /// </summary>
+        /// <param name="appId">The ID of the app from which to get reports.</param>
+        /// <returns>A <see cref="IGetReportsByAppRequestBuilder"/> for further configuration of the request.</returns>
         IGetReportsByAppRequestBuilder FromApp(int appId);
     }
 }
--- Onspring.API.SDK/Models/Fluent/Reports/GetReportsRequestBuilderOptions.cs
+++ Onspring.API.SDK/Models/Fluent/Reports/GetReportsRequestBuilderOptions.cs
@@ -1,10 +1,18 @@
-using Onspring.API.SDK.Enums;
-
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents the options for a request to get reports.
+    /// </summary>
     public class GetReportsRequestBuilderOptions
     {
+        /// <summary>
+        /// Gets or sets the page number to retrieve.
+        /// </summary>
         public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Gets or sets the page size to retrieve.
+        /// </summary>
         public int PageSize { get; set; } = 50;
     }
 }
