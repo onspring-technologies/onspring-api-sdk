861628d351b8e4abcbe5e8cac4faa4281e49db4a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 10:46:47 -0500

    docs: add comments to GetRecordsRequestBuilder

--- Onspring.API.SDK/Models/Fluent/Records/Get/GetRecordsRequestBuilder.cs
+++ Onspring.API.SDK/Models/Fluent/Records/Get/GetRecordsRequestBuilder.cs
@@ -2,10 +2,18 @@ using Onspring.API.SDK.Interfaces.Fluent;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to retrieve records.
+    /// </summary>
+    /// <inheritdoc/>
     public class GetRecordsRequestBuilder : IGetRecordsRequestBuilder
     {
         private readonly IOnspringClient _client;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetRecordsRequestBuilder"/> class.
+        /// </summary>
+        /// <param name="client">The Onspring client to use for making API requests.</param>
         internal GetRecordsRequestBuilder(IOnspringClient client)
         {
             _client = client;
