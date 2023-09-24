3db3a549b5bc4b0cde157eb2137a83f3c7a14b41
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:48:17 -0500

    docs: add comments to DeleteRecordsByAppRequestBuilder

--- Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsByAppRequestBuilder.cs
+++ Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsByAppRequestBuilder.cs
@@ -3,11 +3,20 @@ using Onspring.API.SDK.Interfaces.Fluent;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete records from an app.
+    /// </summary>
+    /// <inheritdoc/>
     public class DeleteRecordsByAppRequestBuilder : IDeleteRecordsByAppRequestBuilder
     {
         private readonly IOnspringClient _client;
         public int AppId { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="DeleteRecordsByAppRequestBuilder"/> class.
+        /// </summary>
+        /// <param name="client">The <see cref="IOnspringClient"/> used to send the request.</param>
+        /// <param name="appId">The ID of the app from which to delete records.</param>
         internal DeleteRecordsByAppRequestBuilder(IOnspringClient client, int appId)
         {
             _client = client;
