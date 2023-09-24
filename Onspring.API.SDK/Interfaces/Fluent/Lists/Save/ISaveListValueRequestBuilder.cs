f3cfdbe913b78e722bf68b05292821d1ea3332a4
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:38:39 -0500

    docs: add comments to SaveListValueRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Lists/Save/ISaveListValueRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Lists/Save/ISaveListValueRequestBuilder.cs
@@ -1,7 +1,15 @@
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to save a list value.
+    /// </summary>
     public interface ISaveListValueRequestBuilder
     {
+        /// <summary>
+        /// Specifies the ID of the list to save the value in.
+        /// </summary>
+        /// <param name="listId">The ID of the list to save the value in.</param>
+        /// <returns>A <see cref="ISaveListValueInListRequestBuilder"/> for further configuration of the request.</returns>
         ISaveListValueInListRequestBuilder InList(int listId);
     }
 }
