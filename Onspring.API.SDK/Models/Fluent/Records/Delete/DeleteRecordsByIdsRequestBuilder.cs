c6e37d2a042e61b3dbd2a88d569accae44b83d2e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 11:52:08 -0500

    docs: add comments to DeleteRecordsByIdsRequestBuilder

--- Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsByIdsRequestBuilder.cs
+++ Onspring.API.SDK/Models/Fluent/Records/Delete/DeleteRecordsByIdsRequestBuilder.cs
@@ -1,18 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Onspring.API.SDK.Interfaces.Fluent;
 
 namespace Onspring.API.SDK.Models.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete records from an app by IDs.
+    /// </summary>
     public class DeleteRecordsByIdsRequestBuilder : IDeleteRecordsByIdsRequestBuilder
     {
         private readonly IOnspringClient _client;
         public int AppId { get; private set; }
         public IEnumerable<int> RecordIds { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="DeleteRecordsByIdsRequestBuilder"/> class.
+        /// </summary>
+        /// <param name="client">The <see cref="IOnspringClient"/> used to send the request.</param>
+        /// <param name="appId">The ID of the app from which to delete records.</param>
+        /// <param name="recordIds">The IDs of the records to delete.</param>
         internal DeleteRecordsByIdsRequestBuilder(IOnspringClient client, int appId, IEnumerable<int> recordIds)
         {
             _client = client;
