c76efa938a61ee10813729b55c4ac4c89fcccac4
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 13:24:20 -0500

    docs: add comments to GetReportRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Reports/IGetReportRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Reports/IGetReportRequestBuilder.cs
@@ -1,7 +1,15 @@
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to get a report.
+    /// </summary>
     public interface IGetReportRequestBuilder
     {
+        /// <summary>
+        /// Specifies the ID of the report to get.
+        /// </summary>
+        /// <param name="reportId">The ID of the report to get.</param>
+        /// <returns>A <see cref="IGetReportDataRequestBuilder"/> for further configuration of the request.</returns>
         IGetReportDataRequestBuilder FromReport(int reportId);
     }
 }
