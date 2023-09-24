493242c6635dc22f09ad7bc37e626ba0f0bc14e2
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 14:35:36 -0500

    docs: add comments to DeleteFileRequestBuilder

--- Onspring.API.SDK/Interfaces/Fluent/Files/Delete/IDeleteFileByIdRequestBuilder.cs
+++ Onspring.API.SDK/Interfaces/Fluent/Files/Delete/IDeleteFileByIdRequestBuilder.cs
@@ -3,11 +3,30 @@ using Onspring.API.SDK.Models;
 
 namespace Onspring.API.SDK.Interfaces.Fluent
 {
+    /// <summary>
+    /// Represents a builder for constructing requests to delete a file.
+    /// </summary>
     public interface IDeleteFileByIdRequestBuilder
     {
+        /// <summary>
+        /// Gets the ID of the record to delete the file from.
+        /// </summary>
         int RecordId { get; }
+
+        /// <summary>
+        /// Gets the ID of the field to delete the file from.
+        /// </summary>
         int FieldId { get; }
+
+        /// <summary>
+        /// Gets the ID of the file to delete.
+        /// </summary>
         int FileId { get; }
+
+        /// <summary>
+        /// Asynchronously sends the request to delete the file.
+        /// </summary>
+        /// <returns>An awaitable task that returns an <see cref="ApiResponse"/> when complete.</returns>
         Task<ApiResponse> SendAsync();
     }
 }
