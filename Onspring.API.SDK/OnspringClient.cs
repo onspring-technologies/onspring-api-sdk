92e0749773349e069ae58353702c915f1a3fd805
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-08 13:59:48 -0600

    feat: added method for getting all fields in app

--- Onspring.API.SDK/OnspringClient.cs
+++ Onspring.API.SDK/OnspringClient.cs
@@ -516,12 +516,7 @@ namespace Onspring.API.SDK
 
         // ------------------------------------ Client internals ------------------------------------
 
-        /// <summary>
-        /// Gets all pages of a paged response.
-        /// </summary>
-        /// <typeparam name="T">Where T is a paged response.</typeparam>
-        /// <param name="callback">The callback to get a page of the response.</param>
-        /// <returns>An async enumerable of the paged response.</returns>
+        // Common method to use for methods that return paged resposnes
         private async IAsyncEnumerable<ApiResponse<T>> GetAllPagesAsync<T>(Func<int, Task<ApiResponse<T>>> callback) where T : PagedResponse
         {
             var initialResponse = await callback(1);
